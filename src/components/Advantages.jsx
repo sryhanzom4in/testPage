4a8ceaae48b759fa14dd08d786acac993b973758
sryhanzom4in <sryhanzom4in@yandex.ru>
2025-11-27 20:00:16 +0700

    Advantages :heavy_check_mark:

--- src/components/Advantages.jsx
+++ src/components/Advantages.jsx
@@ -1,28 +1,29 @@
 import React from "react";
-
+import compassIcon from "../assets/compassIcon.svg";
+import usersIcon from "../assets/usersIcon.svg";
+import trophyIcon from "../assets/trophyIcon.svg";
 export default function Advantages() {
     return (
         <>
             <div className="font-[Unbounded] text-[32px] font-normal">
                 <div className="h-[140px] text-center font-[Unbounded] text-[32px] font-normal">
-                    Мы помогаем собственникам, первым лицам компаний <br /> и их
-                    командам пройти через изменения и неопределённость <br /> к
-                    новым целям.
+                    Мы помогаем собственникам, первым лицам компаний <br /> и их командам пройти через изменения и неопределённость <br /> к новым целям.
                 </div>
             </div>
-            <div>
-                <div>
-                    <img src="./assets/compass.svg" alt="" />
-                    <div>пройти через неопределённость</div>
+            <div className="flex items-center ">
+                <div className="flex flex-col gap-0.5 justify-center items-center w-[400px] h-[200px]">
+                    <img src={compassIcon} alt="" />
+                    <div className="font-[Unbounded] text-[20px] font-normal text-center w-[80%]">пройти через неопределённость</div>
                 </div>
-                <div>
-                    <img src="" alt="" />
-                    <div>сохранить себя и лучших людей в команде</div>
+                <div className="flex flex-col gap-0.5 justify-center items-center w-[400px] h-[200px]">
+                    <img src={usersIcon} alt="" />
+                    <div className="font-[Unbounded] text-[20px] font-normal text-center w-[80%]">сохранить себя и лучших людей в команде</div>
                 </div>
-                <div>
-                    <img src="" alt="" />
-                    <div>
-                        получить тот результат, ради которого начали перемены
+                {/* <div className="w-0 h-[110px] border border-dashed border-[#2563EB]"></div>  */}
+                <div className="flex flex-col gap-0.5 justify-center items-center w-[400px] h-[200px]">
+                    <img src={trophyIcon} alt="" />
+                    <div className="font-[Unbounded] text-[20px] font-normal text-center w-[80%] h-[60px]">
+                        получить тот <br /> результат, ради которого начали перемены
                     </div>
                 </div>
             </div>
--- src/components/ConsultBanner.jsx
+++ src/components/ConsultBanner.jsx
@@ -1,28 +1,20 @@
 import React from "react";
 import { Fragment } from "react";
+import phone from "../assets/phone.png";
 export default function ConsultBanner() {
     return (
         <>
             <div className="bg-[#2563EB] h-[400px] w-full flex justify-center rounded-4xl font-[Unbounded] relative mt-14 mb-[153px]">
                 <div className="flex flex-col">
                     <div className="flex flex-col gap-6 ">
-                        <div className="text-[#E2E8F0] text-7xl text-center h-[99px] mt-15">
-                            #ПобеждаемКомандой
-                        </div>
-                        <div className="text-[#E2E8F0] text-[32px] h-[73px] text-center">
-                            Образовательная программа | ИИ-ассистент |
-                            Сообщество
-                        </div>
+                        <div className="text-[#E2E8F0] text-7xl text-center h-[99px] mt-15">#ПобеждаемКомандой</div>
+                        <div className="text-[#E2E8F0] text-[32px] h-[73px] text-center">Образовательная программа | ИИ-ассистент | Сообщество</div>
                     </div>
                 </div>
                 <div className="w-[450px] h-[200px] bg-[#FFFFFF] font-[Unbounded] rounded-3xl flex flex-col items-center justify-around absolute bottom-[-100px] left-[415px] border border-[#2563EB] ">
-                    <div className="text-[20px] text-[#2563EB] f  w-[400px] text-center flex justify-center">
-                        Технология создания вовлеченных команд и внедрения
-                        изменений
-                    </div>
-                    <button className="text-[#FFFFFF] bg-[#2563EB] py-2.5 px-6.5 rounded-xl ">
-                        Получить консультацию
-                    </button>
+                    <div className="text-[20px] text-[#2563EB] f  w-[400px] text-center flex justify-center">Технология создания вовлеченных команд и внедрения изменений</div>
+                    <button className="text-[#FFFFFF] bg-[#2563EB] py-2.5 px-6.5 rounded-xl ">Получить консультацию</button>
+                    <img className="absolute right-[-140px] bottom--7" src={phone} alt="phone" />
                 </div>
             </div>
         </>
