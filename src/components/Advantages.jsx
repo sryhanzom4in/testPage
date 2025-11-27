3a16f9a4004c995172c28cbf15bb757817bd9621
sryhanzom4in <sryhanzom4in@yandex.ru>
2025-11-27 18:12:40 +0700

    +consultBanner, start of Advantages component

---
+++ src/components/Advantages.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Advantages() {
+    return (
+        <>
+            <div className="font-[Unbounded] text-[32px] font-normal">
+                <div className="h-[140px] text-center font-[Unbounded] text-[32px] font-normal">
+                    Мы помогаем собственникам, первым лицам компаний <br /> и их командам пройти через изменения и неопределённость <br /> к новым целям.
+                </div>
+            </div>
+            <div>
+                <div>
+                    <img src="./assets/compass.svg" alt="" />
+                    <div>пройти через неопределённость</div>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <div>сохранить себя и лучших людей в команде</div>
+                </div>
+                <div>
+                    <img src="" alt="" />
+                    <div>получить тот результат, ради которого начали перемены</div>
+                </div>
+            </div>
+        </>
+    );
+}
