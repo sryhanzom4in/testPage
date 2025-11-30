064b8a7ed469e7d88e5b5118abddb326c31d90c3
sryhanzom4in <sryhanzom4in@yandex.ru>
2025-11-30 14:56:59 +0700

    Track :heavy_check_mark:

--- src/components/Track.jsx
+++ src/components/Track.jsx
@@ -2,41 +2,78 @@ import React from "react";
 
 export default function Track() {
     return (
-        <div className="font-[Unbounded] mt-40 bg-[#]">
-            <div className="text-[32px]">
-                Вы проходите путь через перемены за руку с <span className="text-[#2563EB]">экспертами-практиками</span>
-            </div>
-            <div>
-                <ul>
-                    <li>
-                        <div></div>
-                        <div></div>
-                    </li>
-                    <li>
-                        <div></div>
-                        <div></div>
-                    </li>
-                    <li>
-                        <div></div>
-                        <div></div>
-                    </li>
-                    <li>
-                        <div></div>
-                        <div></div>
-                    </li>
-                    <li>
-                        <div></div>
-                        <div></div>
-                    </li>
-                    <li>
-                        <div></div>
-                        <div></div>
-                    </li>
-                    <li>
-                        <div></div>
-                        <div></div>
-                    </li>
-                </ul>
+        <div className="font-[Unbounded] mt-40 bg-[#FFFFFF] rounded-3xl flex justify-center items-center h-auto">
+            <div className=" w-[90%] h-[90%] mt-15 mb-15">
+                <div className="text-4xl font-bold">
+                    Вы проходите путь через перемены за руку с <span className="text-[#2563EB]">экспертами-практиками</span>
+                </div>
+                <div className="mt-10">
+                    <ul className="flex flex-col">
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">01</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Постановка цели и KPI-навигация</div>
+                                <div className="text-xl">Где мы и куда дальше? Особенности переходного периода. В чём застряли?</div>
+                            </div>
+                        </li>
+                        <div className="w-0 h-14 border-l "></div>
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">02</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Оценка рисков и шансов на успех</div>
+                                <div className="text-xl">Диагностика потенциала команды и точек напряжения, выявление слепых пятен- системно по технологии.</div>
+                            </div>
+                        </li>
+                        <div className="w-0 h-14 border-l "></div>
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">03</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Создание дорожной карты</div>
+                                <div className="text-xl">Главный инструмент в системном управлении изменениями.</div>
+                            </div>
+                        </li>
+                        <div className="w-0 h-14 border-l "></div>
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">04</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Человеческий фактор и вовлечённость</div>
+                                <div className="text-xl">На кого опереться? С кем расстаться? Как увидеть и выбрать амбассадоров, вовлечь в создание внутреннего сообщества лидеров перемен?</div>
+                            </div>
+                        </li>
+                        <div className="w-0 h-14 border-l "></div>
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">05</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Основные знания</div>
+                                <div className="text-xl">Регулярный календарь встреч: разбор кейсов и новых инструментов, вопросы и обмен опытом.</div>
+                            </div>
+                        </li>
+                        <div className="w-0 h-14 border-l "></div>
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">06</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Работа в мини-группах </div>
+                                <div className="text-xl">Отработка практических навыков, и приминение новых инстументов на практику в свои проекты</div>
+                            </div>
+                        </li>
+                        <div className="w-0 h-14 border-l "></div>
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">07</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Консультация с трекерами</div>
+                                <div className="text-xl">Сверка по сложным и нестандартным запросам с экспертами в нише или узкой теме.</div>
+                            </div>
+                        </li>
+                        <div className="w-0 h-14 border-l "></div>
+                        <li className="flex flex-row gap-8">
+                            <div className="text-[#2563EB] text-5xl">08</div>
+                            <div className="flex flex-col w-[65%]">
+                                <div className="text-2xl font-bold">Эмоциональная связь</div>
+                                <div className="text-xl">Очные и онлайн встречи дя тусовок, нетворкинга, взаимной поддержки </div>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
